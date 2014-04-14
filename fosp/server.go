2f831032c0038faae1f13bc99dfccdcd9a2952fd
Felix Maurer <Felix.Maurer@gmx.net>
2014-04-14 16:35:09 +0200

    Document server methods

--- fosp/server.go
+++ fosp/server.go
@@ -28,11 +28,16 @@ var lg = logging.MustGetLogger("go-fosp/fosp")
 type server struct {
 	database    *database
 	connections map[string][]*connection
-	connsLock   sync.Mutex
-	domain      string
-	lg          *logging.Logger
+	// BUG(maufl): Should use RWLock and also lock on reading
+	connsLock sync.Mutex
+	domain    string
+	lg        *logging.Logger
 }
 
+// NewServer initializes a new server struct and returns it.
+// The DatabaseDriver will be used by this server to store data.
+// The server will process requests for resources on the provided domain.
+// The RequestHandler method of the server can be used as a handler for incoming WebSocket connections
 func NewServer(dbDriver DatabaseDriver, domain string) *server {
 	if dbDriver == nil {
 		panic("Cannot initialize server without database")
@@ -45,6 +50,8 @@ func NewServer(dbDriver DatabaseDriver, domain string) *server {
 	return s
 }
 
+// The RequestHandler method accepts a HTTP request and tries to upgrade it to a WebSocket connection.
+// On success it instanciates a new fosp.Connection using the new WebSocket connection.
 func (s *server) RequestHandler(res http.ResponseWriter, req *http.Request) {
 	ws, err := websocket.Upgrade(res, req, nil, 1024, 104)
 	if _, ok := err.(websocket.HandshakeError); ok {
@@ -58,12 +65,16 @@ func (s *server) RequestHandler(res http.ResponseWriter, req *http.Request) {
 	NewConnection(ws, s)
 }
 
+// registerConnection registers a connection with the server for a remote entity.
+// The server saves this connection to it's mapping and associates it with the given remote entity.
 func (s *server) registerConnection(c *connection, remote string) {
 	s.connsLock.Lock()
 	s.connections[remote] = append(s.connections[remote], c)
 	s.connsLock.Unlock()
 }
 
+// Unregister removes an connection from the list of known connections of this server.
+// When no such connection is known by the server then this is a nop.
 func (s *server) Unregister(c *connection, remote string) {
 	s.connsLock.Lock()
 	for i, v := range s.connections[remote] {
@@ -75,6 +86,10 @@ func (s *server) Unregister(c *connection, remote string) {
 	s.connsLock.Unlock()
 }
 
+// routeNotification routes a notification to a user.
+// It first determins if the user belongs to the domain of the server.
+// If that's the case, it searches it's known connection for a connection to this user and sends the notification.
+// Else it routes the notification to a remote server, opening a new connection if necessary.
 func (s *server) routeNotification(user string, notf *Notification) {
 	s.lg.Info("Sending notification %v to user %s", notf, user)
 	if strings.HasSuffix(user, "@"+s.domain) {
@@ -100,6 +115,8 @@ func (s *server) routeNotification(user string, notf *Notification) {
 	}
 }
 
+// forwardRequest sends a request to a remote server and returns the response or an error.
+// It is used to forward a request from a local user for a non local resources to remote servers.
 func (s *server) forwardRequest(user string, rt RequestType, url *Url, headers map[string]string, body []byte) (*Response, error) {
 	remote_domain := url.Domain()
 	headers["User"] = user
@@ -118,6 +135,10 @@ func (s *server) forwardRequest(user string, rt RequestType, url *Url, headers m
 	}
 }
 
+// getOrOpenRemoteConnection returns a connection to the remote_domain.
+// If such a connection already exists and is known to the server, it is reused.
+// Otherwise a new connection is opened.
+// If a new connection is opened, the call will be blocked until the new connection is authenticated or failed.
 func (s *server) getOrOpenRemoteConnection(remote_domain string) (*connection, error) {
 	if connections, ok := s.connections["@"+remote_domain]; ok {
 		for _, connection := range connections {
@@ -127,6 +148,7 @@ func (s *server) getOrOpenRemoteConnection(remote_domain string) (*connection, e
 	return OpenConnection(s, remote_domain)
 }
 
+// Domain returns the domain this server handles.
 func (s *server) Domain() string {
 	if s.domain == "" {
 		return "localhost.localdomain"

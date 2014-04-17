92f38a0ff05ce7d5814de38ca204a85f935fc0a2
Felix Maurer <Felix.Maurer@gmx.net>
2014-04-17 16:49:03 +0200

    Add list and create command to fospc

--- fospc/fospc.go
+++ fospc/fospc.go
@@ -16,20 +16,22 @@
 package main
 
 import (
+	"flag"
 	"github.com/maufl/go-fosp/fosp"
-	"github.com/shavac/readline"
 	"github.com/op/go-logging"
-	"strings"
+	"github.com/shavac/readline"
 	"os"
+	"strings"
 )
 
 type stateStruct struct {
+	Remote   string
 	Username string
 	Password string
-	Cwd string
+	Cwd      string
 }
 
-var state = stateStruct{"<anonymous>", "", "<nowhere>"}
+var state = stateStruct{"", "", "", ""}
 var prompt = state.Username + " @ " + state.Cwd + " >"
 var client = fosp.Client{}
 
@@ -63,10 +65,22 @@ func loop() {
 			println()
 			return
 		case *result != "": //ignore blank lines
-			parseCommand(*result);
-			readline.AddHistory(*result); //allow user to recall this line
+			parseCommand(*result)
+			readline.AddHistory(*result) //allow user to recall this line
 		}
 	}
+}
+
+func buildPrompt() {
+	prompt = state.Username + " @ " + state.Cwd + " >"
+}
+
+func determinURL(path string) (*fosp.URL, error) {
+	url, err := fosp.ParseURL(path)
+	if err != nil {
+		url, err = fosp.ParseURL(state.Cwd + "/" + path)
+	}
+	return url, err
 }
 
 func parseCommand(input string) {
@@ -74,7 +88,9 @@ func parseCommand(input string) {
 	tokens := strings.SplitN(input, " ", 2)
 	cmd := tokens[0]
 	args := ""
-	if len(tokens) == 2 { args = tokens[1] }
+	if len(tokens) == 2 {
+		args = tokens[1]
+	}
 
 	switch cmd {
 	case "quit":
@@ -87,6 +103,10 @@ func parseCommand(input string) {
 		authenticate(args)
 	case "select":
 		selekt(args)
+	case "list":
+		list(args)
+	case "create":
+		create(args)
 	default:
 		println("Unknown command " + cmd)
 	}
@@ -95,6 +115,8 @@ func parseCommand(input string) {
 func open(args string) {
 	if err := client.OpenConnection(args); err != nil {
 		println(err.Error())
+	} else {
+		state.Remote = args
 	}
 }
 
@@ -114,18 +136,60 @@ func authenticate(args string) {
 	if len(parts) != 2 {
 		println("Not enough arguments for authenticate")
 	}
-	client.Authenticate(parts[0], parts[1])
+	if resp, err := client.Authenticate(parts[0], parts[1]); err == nil && resp.ResponseType() == fosp.Succeeded {
+		state.Username = parts[0]
+		state.Password = parts[0]
+		state.Cwd = state.Username + "@" + state.Remote
+		buildPrompt()
+	}
 }
 
 func selekt(args string) {
-	url, err := fosp.ParseURL(args)
+	url, err := determinURL(args)
 	if err != nil {
-		println(args + " is not a valid URL")
+		println(args + " is not a valid path")
 		return
 	}
 	if resp, err := client.Select(url); err == nil {
 		println(resp.BodyString())
 	} else {
 		println("Select failed: " + err.Error())
+	}
+}
+
+func list(args string) {
+	url, err := determinURL(args)
+	if err != nil {
+		println(args + " is not a valid path")
+		return
+	}
+	if resp, err := client.List(url); err == nil {
+		println(resp.BodyString())
+	} else {
+		println("Select failed: " + err.Error())
+	}
+}
+
+func create(args string) {
+	tokens := strings.SplitN(args, " ", 2)
+	path := tokens[0]
+	content := ""
+	if len(tokens) == 2 {
+		content = tokens[1]
+	}
+	url, err := determinURL(path)
+	if err != nil {
+		println(path + " is not a valid path")
+		return
+	}
+	obj, err := fosp.Unmarshal(content)
+	if err != nil {
+		println(content + " is not a valid FOSP object")
+		return
+	}
+	if _, err := client.Create(url, obj); err == nil {
+		println("Create succeeded")
+	} else {
+		println("Create failed: " + err.Error())
 	}
 }
